2eea43eb5dcb59eb089e5d7fb35e5d61e0ab8eb2
Yuri <ygonzoo@gmail.com>
2025-04-29 17:20:18 +0200

    refactor to esm

--- client/index.js
+++ client/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+import io from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
+
+const socket = io("http://localhost:3000");
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
